55cd510969d84b19febd31b6fb0093d68807d152
Bryce Mecum <petridish@gmail.com>
2019-08-28 23:20:36 -0800

    Add in basic EML structure

---
+++ src/schema.js
@@ -0,0 +1,339 @@
+export default {
+  "definitions": {
+    "party": {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string",
+          "title": "Name"
+        },
+        "email": {
+          "type": "string",
+          "title": "Email"
+        },
+        "orcid": {
+          "type": "string",
+          "title": "ORCID"
+        },
+        "role": {
+          "type": "array",
+          "title": "Role(s)",
+          "items": {
+            "type": "string",
+            "enum": [
+              "creator",
+              "contact",
+              "principal_investigator",
+              "co_principal_investigator",
+              "collaborating_principal_investigators",
+              "metadata_provider",
+              "custodian/stewards",
+              "publisher",
+              "user"
+            ],
+            "enumNames": [
+              "Creator",
+              "Contact",
+              "Principal Investigator",
+              "Co-Principal Investigator",
+              "Collaborating Principal Investigator",
+              "Metadata Provider",
+              "Custodian/Steward",
+              "Publisher",
+              "Contributor"
+            ]
+          }
+        }
+      }
+    },
+    "geographic_coverage": {
+      "title": "Geographic Coverage",
+      "type": "object",
+      "properties": {
+        "description": {
+          "type": "string",
+          "title": "Description"
+        },
+        "choice": {
+          "type": "string",
+          "title": "Point or Bounding Box?",
+          "enum": [
+            "Point",
+            "Bounding Box"
+          ]
+        }
+      },
+      "dependencies": {
+        "choice": {
+          "oneOf": [
+            {
+              "properties": {
+                "choice": {
+                  "enum": [
+                    "Point"
+                  ]
+                },
+                "lat": {
+                  "type": "number",
+                  "title": "Latitude (Degrees)",
+                  "minimum": -90,
+                  "maximum": 90
+                },
+                "lon": {
+                  "type": "number",
+                  "title": "Longitude (Degrees)",
+                  "minimum": -180,
+                  "maximum": 180
+                }
+              }
+            },
+            {
+              "properties": {
+                "choice": {
+                  "enum": [
+                    "Bounding Box"
+                  ]
+                },
+                "north": {
+                  "type": "number",
+                  "title": "North Latitude (Degrees)",
+                  "minimum": -90,
+                  "maximum": 90
+                },
+                "east": {
+                  "type": "number",
+                  "title": "Eastern Longitude (Degrees)",
+                  "minimum": -180,
+                  "maximum": 180
+                },
+                "south": {
+                  "type": "number",
+                  "title": "Southern Latitude (Degrees)",
+                  "minimum": -90,
+                  "maximum": 90
+                },
+                "west": {
+                  "type": "number",
+                  "title": "Western Longitude (Degrees)",
+                  "minimum": -180,
+                  "maximum": 180
+                }
+              }
+            }
+          ]
+        }
+      }
+    },
+    "temporal_coverage": {
+      "title": "Temporal Coverage",
+      "type": "object",
+      "properties": {
+        "choice": {
+          "type": "string",
+          "title": "Single Point in Time or Range?",
+          "enum": [
+            "Single Date",
+            "Date Range"
+          ]
+        }
+      },
+      "dependencies": {
+        "choice": {
+          "oneOf": [
+            {
+              "properties": {
+                "choice": {
+                  "enum": [
+                    "Single Date"
+                  ]
+                },
+                "date": {
+                  "type": "string",
+                  "format": "date"
+                }
+              }
+            },
+            {
+              "properties": {
+                "choice": {
+                  "enum": [
+                    "Date Range"
+                  ]
+                },
+                "Start": {
+                  "type": "string",
+                  "format": "date"
+                },
+                "End": {
+                  "type": "string",
+                  "format": "date"
+                }
+              }
+            }
+          ]
+        }
+      }
+    },
+    "taxonomic_classification": {
+      "type": "object",
+      "properties": {
+        "rank": {
+          "type": "string",
+          "title": "Rank",
+          "enum": [
+            "Domain",
+            "Kingdom",
+            "Phylum",
+            "Class",
+            "Order",
+            "Family",
+            "Genus",
+            "Species",
+            "Other"
+          ]
+        },
+        "value": {
+          "type": "string",
+          "title": "Value"
+        }
+      },
+      "dependencies": {
+        "rank": {
+          "oneOf": [
+            {
+              "properties": {
+                "rank": {
+                  "enum": [
+                    "Other"
+                  ]
+                },
+                "other": {
+                  "type": "string",
+                  "title": "Custom Rank"
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+  },
+  "title": "Dataset",
+  "type": "object",
+  "required": ["title"],
+  "properties": {
+    "title": {
+      "type": "string",
+      "title": "Title"
+    },
+    "abstract": {
+      "type": "string",
+      "title": "Abstract"
+    },
+    "keywords": {
+      "type": "array",
+      "title": "Keywords",
+      "items": {
+        "type": "string"
+      }
+    },
+    "funding": {
+      "type": "array",
+      "title": "Funding",
+      "items": {
+        "type": "string"
+      }
+    },
+    "publication_date": {
+      "type": "string",
+      "title": "Publication Date",
+      "format": "date"
+    },
+    "usage": {
+      "type": "string",
+      "title": "Usage Rights",
+      "enum": [
+        "cc-0",
+        "cc-by"
+      ],
+      "enumNames": [
+        "CC-O",
+        "CC-BY"
+      ]
+    },
+    "alternate_identifiers": {
+      "type": "array",
+      "title": "Alternate Identifier(s)",
+      "items": {
+        "type": "string"
+      }
+    },
+    "people": {
+      "type": "array",
+      "title": "People",
+      "items": {
+        "$ref": "#/definitions/party"
+      }
+    },
+    "geographic_coverage": {
+      "type": "array",
+      "title": "Geographic Coverage",
+      "items": {
+        "$ref": "#/definitions/geographic_coverage"
+      }
+    },
+    "temporal_coverage": {
+      "type": "array",
+      "title": "Temporal Coverage",
+      "items": {
+        "$ref": "#/definitions/temporal_coverage"
+      }
+    },
+    "taxonomic_coverage": {
+      "type": "object",
+      "title": "Taxonomic Coverage",
+      "properties": {
+        "general_taxonomic_coverage": {
+          "type": "string",
+          "title": "General Taxonomic Coverage"
+        },
+        "classifications": {
+          "type": "array",
+          "title": "Classifications",
+          "items": { 
+            "$ref": "#/definitions/taxonomic_classification"
+          }
+        }
+      }
+    },
+    "methods": {
+      "type": "object",
+      "title": "Methods",
+      "properties": {
+        "steps": {
+          "type": "array",
+          "title": "Steps",
+          "items": { 
+            "type": "string"
+          }
+        },
+        "study_extent": {
+          "type": "string",
+          "title": "Study Extent"
+        },
+        "description": {
+          "type": "string",
+          "title": "Description"
+        }
+      }
+    },
+    "files": {
+      "type": "array",
+      "title": "Files",
+      "items": {
+        "type": "string",
+        "format": "data-url"
+      }
+    }
+  }
+};
---
+++ src/uischema.js
@@ -0,0 +1,36 @@
+export default {
+  "usage": {
+    "ui:widget": "radio",
+    "ui:options": {
+      "inline": true
+    }
+  },
+  "abstract": {
+    "ui:widget": "textarea"
+  },
+  "methods": {
+    "steps": {
+      "items": {
+        "ui:widget": "textarea"
+      }
+    },
+    "study_extent": {
+      "ui:widget": "textarea"
+    },
+    "description": {
+      "ui:widget": "textarea"
+    }
+  },
+  "geographic_coverage": {
+    "items": {
+      "description": {
+        "ui:widget": "textarea"
+      }  
+    }
+  },
+  "taxonomic_coverage": {
+    "general_taxonomic_coverage": {
+      "ui:widget": "textarea"
+    }
+  }
+};
